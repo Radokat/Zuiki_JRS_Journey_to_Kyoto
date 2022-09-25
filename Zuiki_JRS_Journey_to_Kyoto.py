257237f54173f81e957c9035d6cbecf47cf879d2
Radokat <69212083+Radokat@users.noreply.github.com>
2022-09-25 14:59:52 +0200

    Added button support.

--- Zuiki_JRS_Journey_to_Kyoto.py
+++ Zuiki_JRS_Journey_to_Kyoto.py
@@ -2,20 +2,32 @@ import pyautogui  # install via pip install pyautogui
 import pygame  # install via pip install pygame
 import threading
 import queue
+import os
 
 pyautogui.PAUSE = 0.0571  # Lowest possible delay before the game starts dropping key presses.
 pygame.init()
-
 clock = pygame.time.Clock()
+
 joysticks = [pygame.joystick.Joystick(x) for x in range(pygame.joystick.get_count())]
 joystick_list = []
 joyid = "joyid"
 joyname = "joyname"
 
-q1 = queue.Queue()
-
+qMascon = queue.Queue()
+qButton = queue.Queue()
+notchfix = False
 
 # Define functions
+
+def menu():
+    clearscreen()
+    print('#####################################################################################')
+    print('# Start the game and use the EB notch to sync controller once you are in the train.')
+    print('# Press CTRL + C to end the script.')
+    print(f'# Notchfix enabled: {notchfix}')
+
+def clearscreen():
+    os.system('clear')
 
 
 def brake_inc():
@@ -55,6 +67,56 @@ def power_max():
     pyautogui.keyUp('z')
 
 
+def button_a():
+    pyautogui.keyDown('enter')
+    pyautogui.keyUp('enter')
+
+
+def button_b():
+    pyautogui.keyDown('esc')
+    pyautogui.keyUp('esc')
+
+
+def button_y():
+    pyautogui.keyDown('z')
+    pyautogui.keyUp('z')
+
+
+def button_l():
+    pyautogui.keyDown('l')
+    pyautogui.keyUp('l')
+
+
+def button_r():
+    pyautogui.keyDown('r')
+    pyautogui.keyUp('r')
+
+
+def button_dpad_left():
+    pyautogui.keyDown('left')
+    pyautogui.keyUp('right')
+
+
+def button_dpad_right():
+    pyautogui.keyDown('right')
+    pyautogui.keyUp('right')
+
+
+def button_dpad_up():
+    pyautogui.keyDown('up')
+    pyautogui.keyUp('up')
+
+
+def button_dpad_down():
+    pyautogui.keyDown('down')
+    pyautogui.keyUp('down')
+
+
+def button_pause():
+    pyautogui.keyDown('tab')
+    pyautogui.keyUp('tab')
+
+
 for i in range(pygame.joystick.get_count()):
     jid = {joyid: i, joyname: pygame.joystick.Joystick(i).get_name()}
     joystick_list.append(jid)
@@ -67,149 +129,157 @@ if mascon_select is None:
 
 mascon_counter = 99
 pygame.event.clear()  # Clear events to remove wrong inputs.
+clearscreen()
+print('Please enter the number of the line you want to drive and confirm with enter:')
+print('1:   Kurama Line')
+print('2:   Eizan Line')
 
-print('#########################################################')
-print('Start the game and use the EB notch to sync controller.')
-print('Press CTRL + C to end the script.')
-print('#########################################################')
+rtselect = input()
+if rtselect == '2':
+    notchfix = True
+clearscreen()
+menu()
 
-# Queue worker
+# Queue worker functions
 
 
-def worker():
+def wmascon():
     while True:
-        item = q1.get()
-        item()
-        q1.task_done()
+        item1 = qMascon.get()
+        item1()
+        qMascon.task_done()
+
+
+def wbutton():
+    while True:
+        item2 = qButton.get()
+        item2()
+        qButton.task_done()
 
 
 try:
-    threading.Thread(target=worker, daemon=True).start()
+    threading.Thread(target=wmascon, daemon=True).start()
+    threading.Thread(target=wbutton, daemon=True).start()
 
     while 1:
         for event in pygame.event.get():
-            mascon_axis = (joysticks[mascon_select].get_axis(1))
-            mascon_axis = (round(mascon_axis, 2))
-            # Disabled P5 and original P4 behaviour, as it's not needed.
-            # if mascon_axis == 1.0:
-            #      # print("P5")
-            #    pyautogui.keyDown('z')
-            #    pyautogui.keyUp('z')
-            #    mascon_counter = 0
-            # if mascon_axis == 0.8:
-            #      # print("P4")
-            #    if mascon_counter == 2:
-            #        pyautogui.keyDown('x')  # Increase
-            #        pyautogui.keyUp('x')
-            #        mascon_counter = 1
-            #    if mascon_counter == 0:
-            #        pyautogui.keyDown('d')  # Decrease
-            #        pyautogui.keyUp('d')
-            #        mascon_counter = 1
-            if mascon_axis == 0.8:
-                print("P4")
-                q1.put(power_max)
-                mascon_counter = 1
-            if mascon_axis == 0.62:
-                print("P3")
-                if mascon_counter == 3:
-                    q1.put(power_inc)
-                    mascon_counter = 2
-                if mascon_counter == 1:
-                    q1.put(power_dec)
-                    mascon_counter = 2
-            if mascon_axis == 0.44:
-                print("P2")
-                if mascon_counter == 4:
-                    q1.put(power_inc)
-                    mascon_counter = 3
-                if mascon_counter == 2:
-                    q1.put(power_dec)
-                    mascon_counter = 3
-            if mascon_axis == 0.25:
-                print("P1")
-                if mascon_counter == 5:
-                    q1.put(power_inc)
-                    mascon_counter = 4
-                if mascon_counter == 3:
-                    q1.put(power_dec)
-                    mascon_counter = 4
-            if mascon_axis == 0.0:
-                print("N")
-                q1.put(neutral)
-                mascon_counter = 5
-            if mascon_axis == -0.21:
-                print("B1")
-                if mascon_counter == 5:
-                    q1.put(brake_inc)
-                    mascon_counter = 6
-                if mascon_counter == 7:
-                    q1.put(brake_dec)
-                    mascon_counter = 6
-            if mascon_axis == -0.32:
-                print("B2")
-                if mascon_counter == 6:
-                    q1.put(brake_inc)
-                    mascon_counter = 7
-                if mascon_counter == 8:
-                    q1.put(brake_dec)
-                    mascon_counter = 7
-            if mascon_axis == -0.43:
-                print("B3")
-                if mascon_counter == 7:
-                    q1.put(brake_inc)
-                    mascon_counter = 8
-                if mascon_counter == 9:
-                    q1.put(brake_dec)
-                    mascon_counter = 8
-            if mascon_axis == -0.53:
-                print("B4")
-                if mascon_counter == 8:
-                    q1.put(brake_inc)
-                    mascon_counter = 9
-                if mascon_counter == 10:
-                    q1.put(brake_dec)
-                    mascon_counter = 9
-            if mascon_axis == -0.64:
-                print("B5")
-                if mascon_counter == 9:
-                    q1.put(brake_inc)
-                    mascon_counter = 10
-                if mascon_counter == 11:
-                    q1.put(brake_dec)
-                    mascon_counter = 10
-            if mascon_axis == -0.75:
-                print("B6")
-                if mascon_counter == 10:
-                    q1.put(brake_inc)
-                    mascon_counter = 11
-                if mascon_counter == 12:
-                    q1.put(brake_dec)
-                    mascon_counter = 11
-            if mascon_axis == -0.85:
-                print("B7")
-                if mascon_counter == 11:
-                    q1.put(brake_inc)
-                    mascon_counter = 12
-                if mascon_counter >= 13:
-                    q1.put(brake_dec)
-                    mascon_counter = 12
-            # if mascon_axis == -0.96:
-            #      # print("B8")
-            #      #
-            #    if mascon_counter == 12:
-            #        # pyautogui.keyDown('k')  # Increase
-            #        # pyautogui.keyUp('k')
-            #        mascon_counter = 13
-            #    if mascon_counter == 14:
-            #        # pyautogui.keyDown('j')  # Decrease
-            #        # pyautogui.keyUp('j')
-            #        mascon_counter = 13
-            if mascon_axis == -1.00:
-                print("EB")
-                q1.put(brake_eb)
-                mascon_counter = 14
-        pygame.time.Clock().tick_busy_loop(60)
+            if event.type == pygame.JOYAXISMOTION:
+                # Lever section
+                mascon_axis = (joysticks[mascon_select].get_axis(1))
+                mascon_axis = (round(mascon_axis, 2))
+                if mascon_axis == 0.8:
+                    #print("P4")
+                    qMascon.put(power_max)
+                    mascon_counter = 1
+                if mascon_axis == 0.62:
+                    if mascon_counter == 3:
+                        qMascon.put(power_inc)
+                        mascon_counter = 2
+                    if mascon_counter == 1:
+                        qMascon.put(power_dec)
+                        mascon_counter = 2
+                if mascon_axis == 0.44:
+                    if mascon_counter == 4:
+                        qMascon.put(power_inc)
+                        mascon_counter = 3
+                    if mascon_counter == 2:
+                        qMascon.put(power_dec)
+                        mascon_counter = 3
+                if mascon_axis == 0.25:
+                    if mascon_counter == 5:
+                        qMascon.put(power_inc)
+                        mascon_counter = 4
+                    if mascon_counter == 3:
+                        qMascon.put(power_dec)
+                        mascon_counter = 4
+                if mascon_axis == 0.0:
+                    qMascon.put(neutral)
+                    mascon_counter = 5
+                if mascon_axis == -0.21:
+                    if mascon_counter == 5:
+                        qMascon.put(brake_inc)
+                        mascon_counter = 6
+                    if mascon_counter == 7:
+                        qMascon.put(brake_dec)
+                        if notchfix:
+                            qMascon.put(brake_dec)
+                        mascon_counter = 6
+                if mascon_axis == -0.32:
+                    if mascon_counter == 6:
+                        qMascon.put(brake_inc)
+                        if notchfix:
+                            qMascon.put(brake_inc)
+                        mascon_counter = 7
+                    if mascon_counter == 8:
+                        qMascon.put(brake_dec)
+                        mascon_counter = 7
+                if mascon_axis == -0.43:
+                    if mascon_counter == 7:
+                        qMascon.put(brake_inc)
+                        mascon_counter = 8
+                    if mascon_counter == 9:
+                        qMascon.put(brake_dec)
+                        if notchfix:
+                            qMascon.put(brake_dec)
+                        mascon_counter = 8
+                if mascon_axis == -0.53:
+                    if mascon_counter == 8:
+                        qMascon.put(brake_inc)
+                        if notchfix:
+                            qMascon.put(brake_inc)
+                        mascon_counter = 9
+                    if mascon_counter == 10:
+                        qMascon.put(brake_dec)
+                        mascon_counter = 9
+                if mascon_axis == -0.64:
+                    if mascon_counter == 9:
+                        qMascon.put(brake_inc)
+                        mascon_counter = 10
+                    if mascon_counter == 11:
+                        qMascon.put(brake_dec)
+                        mascon_counter = 10
+                if mascon_axis == -0.75:
+                    if mascon_counter == 10:
+                        qMascon.put(brake_inc)
+                        if notchfix:
+                            qMascon.put(brake_inc)
+                        mascon_counter = 11
+                    if mascon_counter == 12:
+                        qMascon.put(brake_dec)
+                        mascon_counter = 11
+                if mascon_axis == -0.85:
+                    if mascon_counter == 11:
+                        qMascon.put(brake_inc)
+                        mascon_counter = 12
+                    if mascon_counter >= 13:
+                        qMascon.put(brake_dec)
+                        mascon_counter = 12
+                if mascon_axis == -1.00:
+                    qMascon.put(brake_eb)
+                    mascon_counter = 14
+
+            # Button section
+            if event.type == pygame.JOYBUTTONUP and event.button == 0:
+                qButton.put(button_a)
+            if event.type == pygame.JOYBUTTONUP and event.button == 1:
+                qButton.put(button_b)
+            if event.type == pygame.JOYBUTTONUP and event.button == 3:
+                qButton.put(button_y)
+            if event.type == pygame.JOYBUTTONUP and event.button == 5:
+                qButton.put(button_pause)
+            if event.type == pygame.JOYBUTTONUP and event.button == 9:
+                qButton.put(button_l)
+            if event.type == pygame.JOYBUTTONUP and event.button == 10:
+                qButton.put(button_r)
+            if event.type == pygame.JOYBUTTONUP and event.button == 11:
+                qButton.put(button_dpad_up)
+            if event.type == pygame.JOYBUTTONUP and event.button == 12:
+                qButton.put(button_dpad_down)
+            if event.type == pygame.JOYBUTTONUP and event.button == 13:
+                qButton.put(button_dpad_left)
+            if event.type == pygame.JOYBUTTONUP and event.button == 14:
+                qButton.put(button_dpad_right)
+
+        clock.tick_busy_loop(60)
 except KeyboardInterrupt:
     pass
-
